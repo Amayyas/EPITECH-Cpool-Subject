83260a2f4f6b378948d60c309b2f6ea832d86e70
Amayyas <amayyas.aouadene@epitech.eu>
2025-05-08 13:23:53 +0200

    [ADD] Create tests for my_put_nbr function to validate integer printing

---
+++ DAY03/tests/tests_my_put_nbr.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** EPITECH Cpool Subject
+** File description:
+** tests_my_put_nbr
+*/
+
+void my_putchar(char c);
+
+int main(void)
+{
+    my_put_nbr(0);
+    my_put_nbr(1);
+    my_put_nbr(555);
+    my_put_nbr(18515);
+    my_put_nbr(-15);
+    my_put_nbr(-2147483648);
+    my_put_nbr(2147483647);
+    return 0;
+}
