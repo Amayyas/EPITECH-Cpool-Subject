4e1214622afb73f93689117a4e0e87114dae9915
Amayyas <amayyas.aouadene@epitech.eu>
2025-05-10 16:38:16 +0200

    [ADD] Implement rush function to draw a rectangle based on given dimensions

---
+++ RUSH01/rush-1-3/main.c
@@ -0,0 +1,93 @@
+/*
+** EPITECH PROJECT, 2025
+** EPITECH-Cpool-Subject
+** File description:
+** main
+*/
+
+#include <unistd.h>
+
+int my_putchar(char c);
+
+static void my_putstr(char *str)
+{
+    while (*str) {
+        my_putchar(*str);
+        str++;
+    }
+}
+
+static int error(int x, int y)
+{
+    if (x < 1 || y < 1) {
+        my_putstr("Invalid size\n");
+        return 84;
+    }
+    return 0;
+}
+
+void put_stars(int x)
+{
+    for (int i = 0; i < x; i++) {
+        my_putchar('B');
+    }
+    my_putchar('\n');
+}
+
+void put_top_line(int x)
+{
+    if (x == 1)
+        my_putchar('B');
+    else {
+        my_putchar('A');
+        for (int i = 2; i < x; i++) {
+            my_putchar('B');
+        }
+        my_putchar('A');
+    }
+    my_putchar('\n');
+}
+
+void put_bottom_line(int x)
+{
+    if (x == 1)
+        my_putchar('B');
+    else {
+        my_putchar('C');
+        for (int i = 2; i < x; i++) {
+            my_putchar('B');
+        }
+        my_putchar('C');
+    }
+    my_putchar('\n');
+}
+
+void put_middle_line(int x)
+{
+    if (x == 1)
+        my_putchar('B');
+    else {
+        my_putchar('B');
+        for (int i = 2; i < x; i++) {
+            my_putchar(' ');
+        }
+        my_putchar('B');
+    }
+    my_putchar('\n');
+}
+
+void rush(int x, int y)
+{
+    if (error(x, y) == 84) {
+        return;
+    }
+    if (y == 1)
+        put_stars(x);
+    else {
+        put_top_line(x);
+        for (int i = 2; i < y; i++) {
+            put_middle_line(x);
+        }
+        put_bottom_line(x);
+    }
+}
